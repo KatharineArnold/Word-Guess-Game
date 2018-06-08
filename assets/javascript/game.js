172c3aabaa73e8347f2686c52e2d2975411ff4f3
Katharine Arnold <katiekhs123@gmail.com>
2018-06-08 13:25:01 -0600

    nothing is working to get reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
   //  container for words that will be played 
   const wordStore = ['snoopie', 'lassie', 'pluto', 'toto', 'benji'];
 
-  // global Variables ok?
+  // global Variables 
   let wordSpaces = [];
   let currentWord = [];
   let remainingGueses = 8;
@@ -36,15 +36,41 @@ $(document).ready(function () {
 
   //  start button // run generate word
   $("#start-button").click(function () {
-    // currentWord = wordStore[Math.floor(Math.random() * wordStore.length)];
-    currentWord = wordStore[0];
+    currentWord = wordStore[Math.floor(Math.random() * wordStore.length)];
+    //  create blank word
+    for (let i = 0; i < currentWord.length; i++) {
+      wordSpaces.push('-');
+      $("#start-button").hide();
+    }
+    // dispaly blank word
+    renderWordSpaces();
+  });
+
+
+  //  start game Over
+  function resetScore() {
+    $("#blankLetters").empty();
+    $("#usedletters").empty();
+    wordSpaces = [];
+    currentWord = [];
+    remainingGueses = 8;
+    // let wins = 0;
+    usedLetters = [];
+    rightWord = [];
+  };
+
+  //  play again button // run generate word
+  $("#play-again").click(function () {
+    resetScore();
+    currentWord = wordStore[Math.floor(Math.random() * wordStore.length)];
     //  display blank word
     for (let i = 0; i < currentWord.length; i++) {
       wordSpaces.push('-');
     }
     renderWordSpaces();
-    console.log(`currentWord: ${currentWord}`);
   });
+
+  
 
   const renderWordSpaces = () => {
     $("#blankLetters").html(wordSpaces);
@@ -53,22 +79,20 @@ $(document).ready(function () {
 
   function runScoreboard() {
     // display guesses left   
-    $("#guessesLeft").text(remainingGueses);
+    $("#guessesLeft").text('Remaining Guesses:' + remainingGueses);
     // //  display # wins each word guessed correctly
-    $("#totalWins").text(wins);
+    $("#totalWins").text(  "Total Wins:" + wins);
     //  letters guessed tracker
-    $("#usedLetters").text(usedLetters);
+    $("#usedLetters").text('Letters Used:' + usedLetters);
   };
 
   runScoreboard();
 
   document.addEventListener('keypress', (event) => {
     let currentGuess = event.key;
-
     if (usedLetters.includes(currentGuess)) {
       return;
     };
-
     if (currentWord.includes(currentGuess)) {
       //    /* check to see if letter appears multiple times */
       for (var j = 0; j < currentWord.length; j++) {
@@ -79,49 +103,46 @@ $(document).ready(function () {
       renderWordSpaces();
     } else {
       usedLetters.push(currentGuess);
+      remainingGueses--;
     }
+    if (remainingGueses <= 0) {
+      $("#win-lose").text("You Lost");
+    }
+   
 
     if (wordSpaces.join('') === currentWord) {
-      $("#win-lose").text("You Win")
+      $("#win-lose").text("You Win");
 
     }
-
     runScoreboard();
-    console.log(wordSpaces);
   });
 
+ // no negative guesses
+  // choose new word automatically
+  // fix so cant press keys until start
+  // sop being able to push buttons after lose
+  // hide play again
+
+  //  start game Over
+  function resetScore() {
+    let wordSpaces = [];
+    let currentWord = [];
+    let remainingGueses = 8;
+    // let wins = 0;
+    let usedLetters = [];
+    let rightWord = [];
+  };
 
 
 
-
-  // //  start game Over
-  // function resetGame(){
-  //   $('#totalWins').empty();
-  //   $('#blankLetters').empty();
-  //   $('#usedLetters').empty();
-  //   $('#guessesLeft').empty();
-  // remainingGuesses
-
-
-  //     // Clear out arrays
-  //     wordSpaces = [];
-  //     usedLetters = [];
-  // };
 
 
   // //  win alert with song/video
   // var keySound = new Audio('./assets/sounds/typewriter-key.wav');
   // //  lose alert with song/video
   // var keySound = new Audio('./assets/sounds/typewriter-key.wav');
-  // if(remainingGuesses <= 0) {}
+ 
 
-  //  //calls all event listeners
-  // function handleWordGame(){
-  //     //all of the functions will be called here
-
-  //   };
-  //   //runs after html
-  //   $(handleWordGame);
 
 
 });
